264824d3bac7f737b0293d2a8e60d84bff811d6b
dikshay <dikshay1992@gmail.com>
2016-07-09 20:30:08 -0700

    tanvi changes

--- Assets/Button_Layout.cs
+++ Assets/Button_Layout.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
 
-public class Button_Layout : MonoBehaviour {
+public class Button_Layout : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject model1;
+    private GameObject btn_2;
+    // Use this for initialization
+    void Start () {
+        // Search for all Children from this ImageTarget with type VirtualButtonBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            // Register with the virtual buttons TrackableBehaviour
+            vbs[i].RegisterEventHandler(this);
+        }
+        model1 = transform.FindChild("TimeCube").gameObject;
+        model1.SetActive(false);
+        //btn_2 = transform.FindChild("cube2").gameObject;
+    }
 
-	// Use this for initialization
-	void Start () {
-		GameObject.Find ("Button_Surface").GetComponent<Renderer> ().material.color = Color.red;
-	}
+    /// Called when the virtual button has just been released:
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Button released!");
+    }
 
-	// Update is called once per frame
-	void Update () {
-		GameObject.Find ("Button_Surface").GetComponent<Renderer> ().material.color = Color.red;
-	}
+    /// Called when the virtual button has just been pressed:
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        model1.SetActive(true);
+        Debug.Log("Button Pressed!!!!!");
+    }
 }
--- Assets/TimeScript.cs
+++ Assets/TimeScript.cs
@@ -7,7 +7,7 @@ public class TimeScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		currentTime = System.DateTime.Now.ToString ("HH:mm:ss");
-		Debug.Log ("Time recorded in start is " + currentTime);
+		// Debug.Log ("Time recorded in start is " + currentTime);
 		GameObject.Find ("Time_text").GetComponent<TextMesh> ().text = currentTime;
 
 	}
@@ -15,7 +15,7 @@ public class TimeScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		currentTime = System.DateTime.Now.ToString ("HH:mm:ss");
-		Debug.Log ("Time recorded in update is " + currentTime);
+		// Debug.Log ("Time recorded in update is " + currentTime);
 		GameObject.Find ("Time_text").GetComponent<TextMesh> ().text = currentTime;
 	}
 }
